2eaaf89d62c5f0149bf217405f71e4f08ecb9760
nlwkobe30 <nlwkobe30@gmail.com>
2021-12-26 00:40:25 +0800

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -82,16 +82,8 @@ const config = {
             title: 'Community',
             items: [
               {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus'
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus'
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus'
+                label: 'Linkedin',
+                href: 'https://www.linkedin.com/in/po-hsiang-wang-24a63a16b/'
               }
             ]
           },
