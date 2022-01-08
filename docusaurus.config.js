a8c2ed5df0de2dcc016f68fef472d99454cd4672
nlwkobe30 <nlwkobe30@gmail.com>
2022-01-08 18:20:59 +0800

    modify label

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -73,7 +73,7 @@ const config = {
             title: 'Notes',
             items: [
               {
-                label: 'Tutorial',
+                label: 'Intro',
                 to: '/docs/intro'
               }
             ]
